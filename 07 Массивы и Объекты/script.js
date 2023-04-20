a949fa4b599cff56569ff62580634a5212ea2f2d
Denis Basov <den4er@mail.ru>
2023-04-20 19:08:14 +0300

    объекты

--- 07 Массивы и Объекты/script.js
+++ 07 Массивы и Объекты/script.js
@@ -1,10 +1,10 @@
-let students = [];
-let nums = [1, 2, 3, 4, 5];
-let numbers = [4, 6, 4, 6, 8, 9, 3, 4, 5, 7, 44, 22, true];
-let random = [56, 'hello', null, true, undefined, false, '', 0, [1,2,3], {a:3, b:4}];
-let fruits = ['Киви', 'Ананас', 'Кокос', 'Апельсин', 'Банан', 'Яблоко'];
-let pets = ['cat', 'dog', 'bat', 'mouse', "pig", "goat", "sheep", "cow", "chicken"];
-let animals = ['ant', 'bison', 'camel', 'duck', 'elephant', "cat", "dog"];
+// let students = [];
+// let nums = [1, 2, 3, 4, 5];
+// let numbers = [4, 6, 4, 6, 8, 9, 3, 4, 5, 7, 44, 22, true];
+// let random = [56, 'hello', null, true, undefined, false, '', 0, [1,2,3], {a:3, b:4}];
+// let fruits = ['Киви', 'Ананас', 'Кокос', 'Апельсин', 'Банан', 'Яблоко'];
+// let pets = ['cat', 'dog', 'bat', 'mouse', "pig", "goat", "sheep", "cow", "chicken"];
+// let animals = ['ant', 'bison', 'camel', 'duck', 'elephant', "cat", "dog"];
 
 //console.log(students);
 //console.log(numbers);
@@ -153,6 +153,149 @@ let random = [56, 'hello', null, true, undefined, false, '', 0, [1,2,3], {a:3, b
 // Array.prototype.splice()
 // let pets = ['cat', 'dog', 'bat', 'mouse', "pig", "goat", "sheep", "cow", "chicken"];
 
-let deleted = pets.splice(3, 4);
-console.log('deleted', deleted);
-console.log('pets', pets);
+// let deleted = pets.splice(3, 0, 'camel', 'elephant');
+// console.log('deleted', deleted);
+// console.log('pets', pets);
+
+// let fruits = ['Киви', 'Ананас', 'Кокос', 'Апельсин', 'Банан', 'Яблоко'];
+// let animals = ['ant', 'bison', 'camel', 'duck', 'elephant', "cat", "dog"];
+// 1 задание
+// получите на основе имеющихся массивов массив ниже
+// 1. ['ant', 'Ананас', 'Кокос', 'camel', 'duck', 'elephant', "cat", "dog"]
+// 2. ['ant', 'Ананас', 'Кокос', 'camel', 'duck', 'elephant', 'Апельсин', "cat", "dog"]
+//    ['ant', 'Ананас', 'Кокос', 'camel', 'duck', 'elephant', 'Апельсин', 'cat', 'dog']
+// animals.splice(1, 1, fruits[1], fruits[2]); 
+// animals.splice(6, 0, fruits[3]);
+// console.log(animals);
+
+/*
+//let newArr = fruits.slice(1, 3);
+animals.splice(1, 1, ...fruits.slice(1, 3));
+console.log(animals);
+
+//let newArr2 = fruits.slice(3, 4);
+animals.splice(6, 0, ...fruits.slice(3, 4));
+console.log(animals);
+*/
+
+
+let students = [];
+let nums = [1, 2, 3, 4, 5];
+let numbers = [4, 6, 4, 6, 8, 9, 3, 4, 5, 7, 44, 22];
+let random = [56, 'hello', null, true, undefined, false, '', 0, [1,2,3], {a:3, b:4}];
+let fruits = ['Киви', 'Ананас', 'Кокос', 'Апельсин', 'Банан', 'Яблоко'];
+let pets = ['cat', 'dog', 'bat', 'mouse', "pig", "goat", "sheep", "cow", "chicken"];
+let animals = ['ant', 'bison', 'camel', 'duck', 'elephant', "cat", "dog"];
+
+// let str = '';
+// for( let fruit of fruits ){
+//     str += `<li>${fruit}</li>`;
+// }
+// document.write('<ul>' + str + '</ul>');
+// Киви Ананас Кокос Апельсин Банан Яблоко
+
+// let str2 = '';
+// for(let i = 1; i < fruits.length ; i++){
+//     //console.log(i, fruits[i]);
+//     str2 += ` ${fruits[i]}`;
+// }
+// console.log(str2);
+
+// let numbers = [4, 6, 4, 6, 8, 9, 3, 4, 5, 7, 44, 22];
+// let sum = 0;
+// for (let number of numbers) {
+//     sum += number;
+// }
+// console.log(sum);
+
+// let summ = 0;
+// for (let i = 0; i < numbers.length; i++){
+//     summ += numbers[i];
+// }
+// console.log(summ);
+
+/*
+const orders = [1, 2, 3, 4, 5];
+orders.pop();
+orders[1] = 9;
+orders.push(1);
+
+let newOrders = orders;
+
+orders = [1,2,3];
+
+console.log(orders);
+*/
+
+let users = [
+    {
+        id: 1,
+        userName: 'Иван',
+        age: 22,
+        country: 'Россия',
+        hobby: ['Спать', 'Есть', 'Гулять', 'Отдыхать'],
+        isAdmin: false
+    },
+    {
+        id: 2,
+        userName: 'Мария',
+        age: 11,
+        country: 'Россия',
+        hobby: 'Читать',
+        isAdmin: false
+    },
+    {
+        id: 3,
+        userName: 'Анна',
+        age: 43,
+        country: 'Россия',
+        hobby: 'Работать',
+        isAdmin: true
+    }   
+];
+// console.log(users);
+// let user1 = users[0];
+// console.log(user1);
+
+// let myHobby = 'hobby';
+// console.log( user1[myHobby] );
+
+// console.log( users[0].hobby );
+
+
+let domVDerevne = {
+    materialWall:'woods',
+    floor:3,
+    windows:5,
+    materialRoof: 'metal',
+    adress:{
+        city:'Okunevo',
+        street:'Cenralnaya',
+        dom: 36,
+    }
+}; 
+
+domVDerevne.windows -= 1;
+domVDerevne.people = 6;
+domVDerevne.adress.corp = 1;
+domVDerevne.adress.dom = 17;
+
+console.log(domVDerevne);
+
+//console.log(domVDerevne);
+// 3 задание
+// выведите строку в массив вида: metal, 3, woods, 5, Cenralnaya
+//console.log(domVDerevne.materialRoof, domVDerevne.floor, domVDerevne.materialWall, domVDerevne.windows);
+// const str = `${domVDerevne.materialRoof}, ${domVDerevne.floor}, ${domVDerevne.materialWall}, ${domVDerevne.windows}`
+// console.log(str);
+// let metal = domVDerevne.materialRoof;
+// let three = domVDerevne.floor;
+// let woods = domVDerevne.materialWall;
+// let five = domVDerevne.windows;
+// let Cenralnaya = domVDerevne.adress.street;
+// let str = metal + ', ' + three + ', ' + woods + ', ' + five + ', ' + Cenralnaya;
+// console.log(str);
+
+//console.log( domVDerevne.adress.city );
+
+
