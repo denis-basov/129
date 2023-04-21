6b176bdb7b2527a36df5a584c7e908652d8ee6d7
Denis Basov <den4er@mail.ru>
2023-04-21 19:09:18 +0300

    функции. начало

---
+++ 08 JS Функции/script.js
@@ -0,0 +1,101 @@
+// 1. Описание функции
+// function sayHello(){
+//     console.log('Hello');
+// }
+
+// 2. Вызов функции
+// sayHello();
+
+// let ch1 = 5;
+// let ch2 = 3;
+// function summ() {
+//     console.log(ch1 + ch2);
+// }
+// summ();
+
+// function sumUp(){
+//     console.log(2+2);
+// }
+// sumUp();
+// sumUp();
+// sumUp();
+
+// let num = 567;
+// let str = String(num); // '567'
+
+// function getSumm(){
+//     //console.log( Number(str[0]) + Number(str[1]) + Number(str[2]) );
+//     console.log( +str[0] + +str[1] + +str[2] );
+// }
+// getSumm();
+
+
+// function sayHello(greet, firstName){
+//     console.log(`${greet}, ${firstName}!!!`);
+// }
+
+// sayHello('Привет', 'Иван');
+// sayHello('Добрый день', 'Анна');
+// sayHello('Здрасти', 'Ирина');
+
+
+// function sum (x, y) {
+//     console.log (x**2+y**2);
+// }
+// sum(3,6);
+
+// function summNum(a,b){
+//     console.log ( `Операция ${a}**2+${b}**2: результат - ${a**2+b**2}` );
+// }
+// summNum(5, 6);
+
+/*
+function getSum(num1, num2){
+    let res = num1 + num2; 
+    return res; 
+}
+let sum1 = getSum(3,5);// 8
+let sum2 = getSum(5,5);// 10
+
+function getSquare(num){
+    return num * num;
+}
+let square1 = getSquare(sum1);
+let square2 = getSquare(sum2);
+
+console.log(square1, square2);
+*/
+
+// function exampleFunc(a, b) {
+//     let str = `У меня в кармане есть ${a} ${b}`;
+//     return str;
+// }
+// let res = exampleFunc(2, 'яблока');
+// console.log(res);
+
+// function getStr(c,d){
+//     return `${c} + ${d} дм`;
+// }
+
+// let result = getStr(5,7);
+// console.log(result);
+
+// function vsyakoe(n1,n2){
+//     let str1 = `${n1} ${n2}`;
+//     let str2 = str1 + 'jjdhhfkfkdfk';
+    
+//     return str2;
+// }
+// let res1 = vsyakoe(2,34);
+// console.log(res1);
+
+// function whatDate (day, month){
+//     let str = `Сегодня ${day} ${month}`;
+//     return str;
+// }
+// let result = whatDate(21, "апреля");
+// //let result = `Сегодня 21 апреля`;
+// console.log(result);
+
+// let result2 = whatDate(11, 'Мая');
+// console.log(result2);
