7274efebb01961a6306eb482ff6ae1bb18745973
Denis Basov <den4er@mail.ru>
2023-04-24 19:09:23 +0300

    функции детально

---
+++ 09 JS Функции детально/script.js
@@ -0,0 +1,36 @@
+// function sayHello(){
+//     let greet = 'Hello, summer';
+//     return greet;
+// }
+// let myGreet = sayHello()
+// console.log(myGreet);
+
+// function sayHello(){
+//     return 'Hello, summer';
+// }
+// console.log( sayHello() );
+
+// let fruit = 'Банан';
+// function printFruit(){
+//     let fruit = 'Ананас';
+//     console.log(fruit);
+// }
+// printFruit();
+// console.log(fruit);
+
+// let fruit = 'Банан';
+// function printFruit(){
+//     console.log(fruit);
+// }
+// printFruit();
+// console.log(fruit);
+
+let age = 19;
+if(age >= 18){
+    let str = 'Заходи';
+    console.log(str);
+}else{
+    let str = 'Уходи';
+    console.log(str);
+}
+console.log(str);
